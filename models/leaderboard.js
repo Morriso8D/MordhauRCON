9a681e6b25b426cca244302f450bdb793cf470c6
Alex Morris <alex@businesscomparison.com>
2021-07-13 18:01:56 +0100

    fixed divide by zero

--- models/leaderboard.js
+++ models/leaderboard.js
@@ -24,7 +24,7 @@ class Leaderboard {
         const killerData = [data.killer.playfab, data.killer.name];
 
         this.mySQL.connect(connection => {
-            connection.query('INSERT INTO leaderboard (playfabid,name,kills,deaths,k_d,created_at,updated_at) VALUES(?,?,1,0,1,NOW(),NOW()) ON DUPLICATE KEY UPDATE id = id, name = VALUES(name), kills = kills + 1, k_d = (NULLIF(kills,0) / deaths),updated_at = NOW(), created_at = created_at', killerData, (error, result, field) => {
+            connection.query('INSERT INTO leaderboard (playfabid,name,kills,deaths,k_d,created_at,updated_at) VALUES(?,?,1,0,1,NOW(),NOW()) ON DUPLICATE KEY UPDATE id = id, name = VALUES(name), kills = kills + 1, k_d = (kills / NULLIF(deaths,0)),updated_at = NOW(), created_at = created_at', killerData, (error, result, field) => {
                 connection.release();
                 if(error) throw error;
                 console.log('ranked kill updated');
@@ -41,7 +41,7 @@ class Leaderboard {
         const killedData = [data.killed.playfab, data.killed.name];
 
         this.mySQL.connect(connection => {
-            connection.query('INSERT INTO leaderboard (playfabid,name,kills,deaths,k_d,created_at,updated_at) VALUES(?,?,0,1,0,NOW(),NOW()) ON DUPLICATE KEY UPDATE id = id, name = VALUES(name), deaths = deaths + 1, k_d = (kills / deaths), updated_at = NOW(), created_at = created_at', killedData, (error, result, field) => {
+            connection.query('INSERT INTO leaderboard (playfabid,name,kills,deaths,k_d,created_at,updated_at) VALUES(?,?,0,1,0,NOW(),NOW()) ON DUPLICATE KEY UPDATE id = id, name = VALUES(name), deaths = deaths + 1, k_d = (NULLIF(kills,0) / deaths), updated_at = NOW(), created_at = created_at', killedData, (error, result, field) => {
                 connection.release();
                 if(error) throw error;
                 console.log('ranked death updated');
@@ -49,6 +49,7 @@ class Leaderboard {
         });
     }
 
+    // Move to seperate validation file
     _validUpdateRank(playfab){
         if(playfab.match(/^[A-Z0-9]{14,16}$/)) return true;
 
